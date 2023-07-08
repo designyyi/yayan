b0c45a442393f1c70ca96861c2e789fa58bfa1c1
吳閎洧 <84391136+hongwei0522@users.noreply.github.com>
2023-07-08 18:00:15 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,25 +1,39 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop:true,
-        items:1,
-        autoplay:true,
-        autoplayTimeout:4000,
-        lazyLoad: true,
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut'
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  // 检查是否已经添加过参数
+  var hasAddedParameter = false;
 
+  // 检查网址是否已经包含参数
+  if (window.location.search.indexOf('openExternalBrowser=1') !== -1) {
+    hasAddedParameter = true;
+  }
 
-    // var menuOpen = document.getElementById("menuOpen");
-    // var menuClose = document.getElementById("menuClose");
-    // var menu = document.getElementById("menu");
+  // 如果没有添加过参数，则添加参数
+  if (!hasAddedParameter) {
+    const url = window.location.href;
+    const modifiedUrl = url + '?openExternalBrowser=1';
+    window.location.href = modifiedUrl;
+  }
 
-    // menuOpen.addEventListener("click", function(){
-    //     $(menu).css('display', 'inline');
-    // });
+  var owl = $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    lazyLoad: true,
+    // animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+  });
+  owl.trigger('to.owl.carousel', [1]);
 
-    // menuClose.addEventListener("click", function(){
-    //     $(menu).css('display', 'none');
-    // });
+  // var menuOpen = document.getElementById("menuOpen");
+  // var menuClose = document.getElementById("menuClose");
+  // var menu = document.getElementById("menu");
 
+  // menuOpen.addEventListener("click", function(){
+  //     $(menu).css('display', 'inline');
+  // });
+
+  // menuClose.addEventListener("click", function(){
+  //     $(menu).css('display', 'none');
+  // });
 });
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,59 +1,57 @@
 // 電腦版 滑鼠效果
 
-var item1 = document.getElementById("item1");
-var item2 = document.getElementById("item2");
-var item3 = document.getElementById("item3");
-var item4 = document.getElementById("item4");
-var item5 = document.getElementById("item5");
-item1.addEventListener('mouseover', over)
-item2.addEventListener('mouseover', over)
-item3.addEventListener('mouseover', over)
-item4.addEventListener('mouseover', over)
-item5.addEventListener('mouseover', over)
-item1.addEventListener('mouseleave', leave)
-item2.addEventListener('mouseleave', leave)
-item3.addEventListener('mouseleave', leave)
-item4.addEventListener('mouseleave', leave)
-item5.addEventListener('mouseleave', leave)
+var item1 = document.getElementById('item1');
+var item2 = document.getElementById('item2');
+var item3 = document.getElementById('item3');
+var item4 = document.getElementById('item4');
+// var item5 = document.getElementById("item5");
+item1.addEventListener('mouseover', over);
+item2.addEventListener('mouseover', over);
+item3.addEventListener('mouseover', over);
+item4.addEventListener('mouseover', over);
+// item5.addEventListener('mouseover', over)
+item1.addEventListener('mouseleave', leave);
+item2.addEventListener('mouseleave', leave);
+item3.addEventListener('mouseleave', leave);
+item4.addEventListener('mouseleave', leave);
+// item5.addEventListener('mouseleave', leave)
 
-function over () {
-    this.children[0].style.transform = 'scale(1.1)';
-    this.children[0].style.opacity = '0.6';
-    // this.children[1].style.marginTop = '12px';
-    this.children[1].style.paddingBottom = '2px';
-    this.children[1].style.opacity = '0.6';
-    this.style.color = '#c59b6d';
+function over() {
+  this.children[0].style.transform = 'scale(1.1)';
+  this.children[0].style.opacity = '0.6';
+  // this.children[1].style.marginTop = '12px';
+  this.children[1].style.paddingBottom = '2px';
+  this.children[1].style.opacity = '0.6';
+  this.style.color = '#c59b6d';
 }
 
-function leave () {
-    this.children[0].style.transform = 'scale(1)';
-    this.children[0].style.opacity = '1';
-    this.children[1].style.marginTop = '0px';
-    this.children[1].style.paddingBottom = '0px';
-    this.children[1].style.opacity = '1';
-    this.style.color = '#604a40';
+function leave() {
+  this.children[0].style.transform = 'scale(1)';
+  this.children[0].style.opacity = '1';
+  this.children[1].style.marginTop = '30px';
+  this.children[1].style.paddingBottom = '0px';
+  this.children[1].style.opacity = '1';
+  this.style.color = '#604a40';
 }
 
 // 手機版輪播效果
 // https://swiperjs.com/api/
 
-$(document).ready(function(){
-
-    var swiper = new Swiper('.swiper-container', {
-      lazy: true,
-      loop: true,
-        effect: 'coverflow',
-        slidesPerView: '2',
-        spaceBetween: 30,
-        centeredSlides: true,
-        initialSlide: 0,
-        mousewheel: true,
-        grabCursor: true,
-        mousewheelControl: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
-  
-  })
+$(document).ready(function () {
+  var swiper = new Swiper('.swiper-container', {
+    lazy: true,
+    loop: true,
+    effect: 'coverflow',
+    slidesPerView: '2',
+    spaceBetween: 30,
+    centeredSlides: true,
+    initialSlide: 0,
+    mousewheel: true,
+    grabCursor: true,
+    mousewheelControl: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+});
